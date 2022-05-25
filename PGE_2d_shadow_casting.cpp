bae0cb2ed7f4fa11d35d90020bc9e691b5fafd96
unknown <a.g.marosi@bluewin.ch>
2022-05-25 14:10:06 +0200

    added ability to select an intersection

--- PGE_2d_shadow_casting.cpp
+++ PGE_2d_shadow_casting.cpp
@@ -4,6 +4,14 @@
 #include <vector>
 #include <string>
 
+struct line
+{
+  int x1;
+  int y1;
+  int x2;
+  int y2;
+};
+
 class PGE_2d_shadow_casting : public olc::PixelGameEngine
 {
 public:
@@ -17,6 +25,8 @@ private:
   int gridSize = 20;
   int controlAreaHeight = 100;
   bool close = false;
+  std::vector<line> lines;
+  std::pair<int, int> selectedIntersection = {-1, -1};
 
 public:
   bool OnUserCreate() override
@@ -30,6 +40,8 @@ public:
   {
     if (IsFocused())
     {
+      // TODO: creation and deletion of lines in grid positions
+      // TODO: modes for creating lines and casting light/shadows
       UserInput();
 
       // Closes the program
@@ -45,6 +57,9 @@ public:
     return true;
   }
 
+  /**
+   * @brief All user input is handeled here
+   */
   void UserInput()
   {
     // Program can be terminated by pressing escape
@@ -52,13 +67,33 @@ public:
     {
       close = true;
     }
+
+    // TODO: check if the intersection isn't occupied by any other line
+    // Selects an intersection provided the mouse is in a valid place
+    if (GetMouseY() > controlAreaHeight && GetMouse(0).bPressed)
+    {
+      selectedIntersection.first = FindClosestMultipleOf20(GetMouseX());
+      selectedIntersection.second = FindClosestMultipleOf20(GetMouseY());
+    }
+
+    // Right click clears the selected intersection
+    if (GetMouse(1).bPressed)
+    {
+      selectedIntersection = {-1, -1};
+    }
   }
 
+  /**
+   * @brief All internal logic happens here
+   */
   void UpdateState()
   {
 
   }
 
+  /**
+   * @brief All drawing routines take place here
+   */
   void DrawToScreen()
   {
     Clear(olc::BLACK);
@@ -69,10 +104,19 @@ public:
     // Drawing a cricle around the intersection closest to the mouse position
     HighlightNearestIntersection();
 
-    // Drawing the control area at the top of the screen
-    FillRect(0, 0, ScreenWidth(), controlAreaHeight, olc::Pixel(115, 70, 120));
+    // Draws the selected intersection
+    if (selectedIntersection.first != -1 && selectedIntersection.second != -1)
+    {
+      FillCircle(selectedIntersection.first * gridSize, selectedIntersection.second * gridSize, circleRadius * 0.66f, olc::MAGENTA);
+    }
+
+    // Draws the control area with text
+    DrawControlArea();
   }
 
+  /**
+   * @brief Draws the grid below the control area
+   */
   void DrawGrid()
   {
     // Grid field is 1280x720px with 100px at the top to display text
@@ -90,7 +134,9 @@ public:
   }
 
   // TODO {optional}: implement deadzone
-  // Highlights the nearest intersection to the mouse position with an orange circle
+  /**
+   * @brief Highlights the nearest intersection to the mouse position with an orange circle
+   */
   void HighlightNearestIntersection()
   {
     if (GetMouseY() > controlAreaHeight)
@@ -99,11 +145,29 @@ public:
       int x = FindClosestMultipleOf20(GetMouseX());
       int y = FindClosestMultipleOf20(GetMouseY());
 
-      DrawCircle(x, y, circleRadius, olc::Pixel(255, 155, 0));
+      DrawCircle(x * gridSize, y * gridSize, circleRadius, olc::Pixel(255, 155, 0));
     }
   }
 
-  // Finds the closest multiple of 20 to the input number
+  /**
+   * @brief Draws the control area at the top of the screen space
+   */
+  void DrawControlArea()
+  {
+    // Drawing the control area at the top of the screen
+    FillRect(0, 0, ScreenWidth(), controlAreaHeight, olc::Pixel(115, 0, 125));
+
+    // Draws some text into the control area
+    // UI scaling: 2
+    DrawString(5, 5, "This is a showcase of 2d shadow casting.", olc::Pixel(0, 255, 255), 2);
+  }
+
+  /**
+   * @brief Finds the closest multiple of 20 to the input number
+   *
+   * @param number The position of the mouse
+   * @return int The grid coordinate of the number
+   */
   int FindClosestMultipleOf20(const int& number)
   {
     int multiplier = number / gridSize;
@@ -115,7 +179,7 @@ public:
     int closestLarger = (multiplier + 1) * gridSize;
 
     // Returning the one which has a smaller absolute distance to the number
-    return (abs(number - closestSmaller) < abs(number - closestLarger)) ? closestSmaller : closestLarger;
+    return (abs(number - closestSmaller) < abs(number - closestLarger)) ? multiplier : multiplier + 1;
   }
 };
 
